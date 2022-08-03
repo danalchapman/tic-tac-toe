21668a9fad97a14e7df297445d8890a2b516248b
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-03 17:58:38 -0500

    Add tentative methods for game.js with pseudocode

--- src/game.js
+++ src/game.js
@@ -1,15 +1,46 @@
-// Methods needed:
-// Check for win conditions: 3 horizontally, vertically, or diagonally (checkWin) (then invoke resetGame)
-// Check for draw condition (checkDraw) (then invoke resetGame)
-// Reset game (resetGame)
-// Track Turns
-
 class Game {
     constructor() {
-        this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        this.turnCount = 0;
-        this.gameOver = false;
-        this.gameDraw = false;
-        this.players = [];
+        this.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]; // id names of buttons
+        this.players = []; // playerone & playertwo
+        this.turnCount = 0; // turns until win
+        this.gameOver = false; // gameOver === true;
+        this.gameDraw = false; // gameDraw === true;
+    }
+
+    checkPlayerTurn() {
+        // playerone === true/false?
+        // currentPlayer = playerone/playertwo?
+    }
+
+    checkHorizontalWin() {
+        // horizontal win conditions include
+        // [0], [1], [2]
+        // [3], [4], [5]
+        // [6], [7], [8]
+    }
+
+    checkVerticalWin() {
+        // vertical win conditions include
+        // [0], [3], [6]
+        // [1], [4], [7]
+        // [2], [5], [8]
+    }
+
+    checkDiagonalWin() {
+        // diagonal win conditions include
+        // [0], [4], [8]
+        // [2], [4], [6]
+    }
+
+    checkWinCondition() {
+        // check hor/vert/diag methods
+    }
+
+    checkDrawCondition() {
+        // check for Draw conditions
+    }
+
+    resetGame() {
+        // timeout?? research
     }
 }
