6ba4c70e236afb47b299978edf7fdfd2b9784c61
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-03 17:18:16 -0500

    Start Game Class constructor with desired properties/conditions

---
+++ src/game.js
@@ -0,0 +1,9 @@
+class Game {
+    constructor() {
+        this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        this.turnCount = 0;
+        this.gameOver = false;
+        this.gameDraw = false;
+        this.players = [];
+    }
+}
