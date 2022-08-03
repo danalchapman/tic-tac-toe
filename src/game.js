b3de91fd948fa1d438f750e7200092005d60dd3e
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-03 17:30:55 -0500

    Adjust id names for board spaces in HTML and uncomment needed methods for game.js

--- src/game.js
+++ src/game.js
@@ -1,3 +1,9 @@
+// Methods needed:
+// Check for win conditions: 3 horizontally, vertically, or diagonally (checkWin) (then invoke resetGame)
+// Check for draw condition (checkDraw) (then invoke resetGame)
+// Reset game (resetGame)
+// Track Turns
+
 class Game {
     constructor() {
         this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
