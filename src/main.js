c7c2c075e5aa7b94d8a6bd2f044ec6189f1ee5b5
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-08-06 16:29:34 -0500

    Add Header Turn Tracker functionality

--- src/main.js
+++ src/main.js
@@ -6,21 +6,37 @@ var currentGame
 // Selectors
 var gameBoard = document.querySelector(".game-board");
 var boardCells = document.querySelectorAll("button");
-var turnCounter = document.querySelector(".player-header");
+var turnCounter = document.querySelector(".turn-counter");
 var player1Wins = document.querySelector(".player1-win-count");
 var player2Wins = document.querySelector(".player2-win-count");
 var announcement = document.querySelector(".announce-banner");
-
 
 // Event Listeners 
 window.addEventListener("load", createGame);
 gameBoard.addEventListener("click", clickBoard);
 
 // Functions
+// New Game
 function createGame() {
     currentGame = new Game(playerOne, playerTwo);
+    showCurrentTurn();
 }
 
+// Turn Display
+function showCurrentTurn() {
+    for (var i = 0; i < currentGame.players.length; i++) {
+        if (currentGame.currentTurn === currentGame.players[i].name) {
+            turnCounter.innerHTML = 
+            `<h2 class="player-header">The <img class="player-token" src="${currentGame.players[i].token}" alt="${currentGame.players[i].name}">'s Turn</h2>`
+        }
+
+        if (currentGame.gameOver) {
+            turnCounter.innerHTML = "";
+        }
+    }
+}
+
+// Game Board
 function clickBoard(event) {
     var cellId = event.target.id; {
     if (currentGame.board[cellId] === "") {
@@ -38,7 +54,7 @@ function clickBoard(event) {
 function renderBoard() { 
     for (var i = 0; i < boardCells.length; i++) {
         var cellId = boardCells[i].id;
-
+        
         if (!currentGame.board[cellId]) {
             boardCells[i].innerHTML = "";
         } else {
@@ -46,11 +62,15 @@ function renderBoard() {
             `<img src=${currentGame.board[cellId]}>`;
         }
     }
+    console.log(currentGame.currentTurn);
+    console.log(currentGame.gameOver)
+    showCurrentTurn();
 }
 
-// turnCounter (change token to indicate turn)
-// swap src=${} dependent on currentTurn
+// Game Over Display
+// function displayAnnouncement() {
 
+// }
 // displayAnnoucement (show the winner or draw/update currentGame.players[i]winsCount = player1-win-count)
 // announceWinner.classList.remove(".hidden")
 // on a draw--
@@ -58,6 +78,7 @@ function renderBoard() {
 // on a winner--
 // <img class="player-token hidden" src="./assets/Sun.svg" alt="player 1 sun"> Wins!
 
+// Reset Game
 function triggerBoardReset() {
     currentGame.resetGame()
     renderBoard();
